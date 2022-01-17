05f0fcce8ebbffa4ff28b230e89a01d303f737ea
g-aneeka <aneekageorge@gmail.com>
2022-01-17 21:38:49 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  formatLabel(value: number) {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return value;
+  }
+
+  public graph = {
+    data : [{
+      type: "sunburst",
+      labels: ["Eve", "Cain", "Seth", "Enos", "Noam", "Abel", "Awan", "Enoch", "Azura"],
+      parents: ["", "Eve", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve" ],
+      values:  [10, 14, 12, 10, 2, 6, 6, 4, 4],
+      outsidetextfont: {size: 20, color: "#377eb8"},
+      leaf: {opacity: 0.4},
+      marker: {line: {width: 2}},
+    }],
+    
+    layout : {
+      margin: {l: 0, r: 0, b: 0, t: 0},
+      width: 500,
+      height: 500
+    }
+};
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+
+import * as PlotlyJS from 'plotly.js-dist-min';
+import { PlotlyModule } from 'angular-plotly.js';
+
+PlotlyModule.plotlyjs = PlotlyJS;
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    PlotlyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
